d6db70b941d66e087761a0f97ebbf0a3e20db305
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-05-31 22:47:12 +0530

    fix: reduce top padding on blog page layout

--- client/src/app/explore/[slug]/page.tsx
+++ client/src/app/explore/[slug]/page.tsx
@@ -94,7 +94,7 @@ export default function BlogPage() {
   if (!blog) return <div className="text-center p-10">Blog not found</div>;
 
   return (
-    <div className="max-w-5xl mx-auto px-6 pt-24 pb-8 space-y-6">
+    <div className="max-w-5xl mx-auto px-6 pt-6 pb-8 space-y-6">
       <div className="flex items-center gap-4">
         <Avatar>
           <AvatarImage src={blog.author.pfp} />
