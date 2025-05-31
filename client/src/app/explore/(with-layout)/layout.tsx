eb720842a0e42c456e955c4dc673d429e72e39ab
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-05-31 22:53:36 +0530

    chore: added mobile responsiveness to blogs slug page

--- client/src/app/explore/(with-layout)/layout.tsx
+++ client/src/app/explore/(with-layout)/layout.tsx
@@ -5,7 +5,7 @@ import type { ReactNode } from "react";
 export default function ExploreLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen w-full flex flex-col items-center">
-      <div className="w-full max-w-5xl px-4 mt-32 space-y-2 ml-7">
+      <div className="w-full max-w-5xl px-8 mt-20 sm:mt-24 space-y-2 mx-auto">
         <h1 className="bigText">Blogs</h1>
         <p className="text-lg text-muted-foreground font-mono">
           Explore a wide range of blogs. All in one place.
