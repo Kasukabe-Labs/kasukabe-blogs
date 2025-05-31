f4b6f6115f836aa51b6ef54235ab8b278c9a1a42
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-05-31 21:21:05 +0530

    chore: added healthRouter inside index.ts

--- server2/src/index.ts
+++ server2/src/index.ts
@@ -5,6 +5,7 @@ import connectDB from "./config/db";
 import cookieParser from "cookie-parser";
 import blogRouter from "./routes/blogRoutes";
 import authRouter from "./routes/authRoutes";
+import { healthRouter } from "./routes/healthRoutes";
 
 const app = express();
 dotenv.config();
@@ -28,6 +29,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRouter);
 app.use("/api/blog", blogRouter);
+app.use("/api/health", healthRouter);
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
